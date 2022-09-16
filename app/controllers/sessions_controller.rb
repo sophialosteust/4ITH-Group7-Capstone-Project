b5ac7b6a525343b7e2e1f0e096658e36af2c95c6
Enzo161 <lorenzo.plantilla.cics@ust.edu.ph>
2022-09-16 15:07:37 +0800

    updated some front-end in the login page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionsController < Devise::SessionsController
+  include AuthenticateWithOtpTwoFactor
+
+  prepend_before_action :authenticate_with_otp_two_factor,
+                        if: -> { action_name == 'create' && otp_two_factor_enabled? }
+
+  protect_from_forgery with: :exception, prepend: true, except: :destroy
+
+end
